308922bb85b556d67d164ea0770cfb48d5745195
Suraiya Supti <suptisuraiya8@gmail.com>
2021-10-15 14:15:10 +0530

    Footer Links Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,16 @@ const Footer = () => {
           href="https://github.com/suraiyasupti/grocery-app"
           target="_blank"
         >
-          View Source on Github
+          Contact Us
         </a>
         <span> / </span>
         <a href="mailto:suptisuraiya8@gmail.com" target="_blank">
-          Need any help?
+          Need Help? Connect with Helpdesk
         </a>
         <span> / </span>
+        <a href="https://www.google.com/maps/place/Federation+University+Australia/@-37.6264367,143.8889065,17z/data=!3m1!4b1!4m5!3m4!1s0x6ad144fa122cca73:0xc0d2bd8d84c65803!8m2!3d-37.626441!4d143.8910952" target="_blank">
+          Locate Us
+        </a>
       </p>
       <p>
         &copy; {currentYear} <strong>Fresh</strong> - Organic Grocery Store
